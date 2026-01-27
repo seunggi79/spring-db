b1be996a9fbc8939890314d490ca10739d0e137f
seunggi <hsg77689469@gmail.com>
2026-01-27 19:41:33 +0900

    테스트-스프링 부트와 임베디드 모드

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -28,7 +28,7 @@ public class ItemServiceApplication {
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
-	@Bean
+	/*@Bean
 	@Profile("test")
 	public DataSource dataSource() {
 		log.info("메모리 데이터베이스 초기화");
@@ -38,6 +38,6 @@ public class ItemServiceApplication {
 		dataSource.setUsername("sa");
 		dataSource.setPassword("");
 		return dataSource;
-	}
+	}*/
 }
 
